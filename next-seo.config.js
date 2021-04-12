b258bf170e29ead9a26f67d9672e0c524ea0339b
Neil Skaria <neil.skaria@gmail.com>
2021-04-12 17:56:57 -0500

    Forgot to change image size in next-seo.config

--- next-seo.config.js
+++ next-seo.config.js
@@ -18,8 +18,8 @@ const SEO = {
         url:
           'https://images.ctfassets.net/v9szi7zqf8b3/5Affmh0x47L5IZ07JbjBet/246d4cffcce41557601ff6593a1d23f0/journey-2021.jpg',
         alt: title,
-        width: 1280,
-        height: 720,
+        width: 450,
+        height: 450,
       },
     ],
   },
